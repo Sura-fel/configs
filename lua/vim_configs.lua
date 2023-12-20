59a70f7c57863e3ade5fbc152c14c7bb8d6941bd
Surafel <fkresurafel@gmail.com>
2023-12-20 11:44:51 +0300

    hybrid line number

--- lua/vim_configs.lua
+++ lua/vim_configs.lua
@@ -3,8 +3,10 @@ vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.wo.number = true
+vim.wo.relativenumber = true
 vim.o.mouse = 'a'
 vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.clipboard = 'unnamedplus'
+
