97fe1942b1b6b32b15430f00839d89982e66514e
Surafel <fkresurafel@gmail.com>
2023-12-28 12:39:38 +0300

    Moved the confs here

--- lua/configs/telescope_config.lua
+++ lua/configs/telescope_config.lua
@@ -1,6 +1,22 @@
-local builtin = require("telescope.builtin") 
+require("telescope").setup({
+    defaults = {
+        file_ignore_patterns = { ".git" },
+        mappings = {
+            i = {
+                ["<C-j>"] = require("telescope.actions").move_selection_next,
+                ["<C-k>"] = require("telescope.actions").move_selection_previous,
+                ["<Esc>"] = require("telescope.actions").close,
+                ["<C-c>"] = require("telescope.actions").close
+            }
+        }
+    }
+})
+
+
+local builtin = require("telescope.builtin")
 
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp Menu' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>fg', "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = "Live Grep"})
 
