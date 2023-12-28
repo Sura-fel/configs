f50f8cf10674b219471cdfc0e5d745fff64309ac
Surafel <fkresurafel@gmail.com>
2023-12-28 12:41:51 +0300

    moved configs to config file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,20 +2,6 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        require("telescope").setup({
-            defaults = {
-                file_ignore_patterns = { ".git" },
-                mappings = {
-                    i = {
-                        ["<C-j>"] = require("telescope.actions").move_selection_next,
-                        ["<C-k>"] = require("telescope.actions").move_selection_previous,
-                        ["<Esc>"] = require("telescope.actions").close,
-                        ["<C-c>"] = require("telescope.actions").close,
-                    },
-                },
-            },
-        })
-    end
+
 }
 
