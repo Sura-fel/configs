bff77f5e399e7ec59dab42c305036698c6f9942c
Surafel <fkresurafel@gmail.com>
2023-12-28 14:14:02 +0300

    remove auto cargo fmt

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,9 +1,6 @@
 return {
     {
-            "rust-lang/rust.vim",
-            ft = "rust",
-            init = function ()
-                vim.g.rustfmt_autosave = 1
-            end
+        "rust-lang/rust.vim",
+        ft = "rust",
     }
 }
