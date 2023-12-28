6e201fee4dbe63b8b8896c89095559b57fcd9e71
Surafel <fkresurafel@gmail.com>
2023-12-28 12:40:42 +0300

    Disabled checkOnSave

--- lua/configs/rust_tools_config.lua
+++ lua/configs/rust_tools_config.lua
@@ -1,18 +1,26 @@
 local rt = require("rust-tools")
 
 rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr, desc = "Hover Actions (RT)" })
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr, desc = "Code [A]ction Group" })
-      vim.keymap.set("n", "<leader>r", ":RustRun<CR>", { buffer = bufnr, desc = "[R]un Rust code" })
+    server = {
+        settings = {
+            ["rust-analyzer"] = {
+                checkOnSave = {
+                    enable = false,
+                },
 
-    end,
-  },
-  tools = {
-    hover_actions = {
-      auto_focus = true
+            },
+        },
+        on_attach = function(_, bufnr)
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr, desc = "Hover Actions (RT)" })
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr, desc = "Code [A]ction Group" })
+            vim.keymap.set("n", "<leader>r", ":RustRun<CR>", { buffer = bufnr, desc = "[R]un Rust code" })
+
+        end,
+    },
+    tools = {
+        hover_actions = {
+            auto_focus = true
+        }
     }
-  }
 })
 
