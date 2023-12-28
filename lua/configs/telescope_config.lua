1ba2f4a2b013e6395ee06e131e0220ef0ce41442
Surafel <fkresurafel@gmail.com>
2023-12-28 13:48:39 +0300

    search buffer

--- lua/configs/telescope_config.lua
+++ lua/configs/telescope_config.lua
@@ -15,8 +15,9 @@ require("telescope").setup({
 
 local builtin = require("telescope.builtin")
 
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp Menu' })
-vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>fg', "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = "Live Grep"})
+vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp Menu" })
+vim.keymap.set("n", "<leader>ss", builtin.grep_string, { desc = "[S]earch Current [S]tring" })
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch Live [G]rep" })
+vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch Open [B]uffers" })
 
