f184c3a537653a6569bf8e2be67eeadfa14b313a
Surafel <fkresurafel@gmail.com>
2023-12-29 15:27:16 +0300

    removed extra line

--- lua/configs/rust_tools_config.lua
+++ lua/configs/rust_tools_config.lua
@@ -7,14 +7,12 @@ rt.setup({
                 checkOnSave = {
                     enable = false,
                 },
-
             },
         },
         on_attach = function(_, bufnr)
             vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr, desc = "Hover Actions (RT)" })
             vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr, desc = "Code [A]ction Group" })
             vim.keymap.set("n", "<leader>r", ":RustRun<CR>", { buffer = bufnr, desc = "[R]un Rust code" })
-
         end,
     },
     tools = {
