d69ad63d535d9e130a8924c3af928cd0c750a823
Surafel <fkresurafel@gmail.com>
2023-12-29 16:37:37 +0300

    resize split

--- lua/vim_configs.lua
+++ lua/vim_configs.lua
@@ -9,6 +9,7 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.clipboard = 'unnamedplus'
 vim.api.nvim_set_keymap('n', '<Leader>t', ':botright split +<CR>:resize 10<CR>:terminal powershell -NoLogo<CR>i', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>T', ':botright vsplit +<CR>:terminal powershell -NoLogo<CR>i', {noremap = true})
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
 vim.api.nvim_set_keymap('t', '<M-h>', '<C-w>h', { noremap = true })
 vim.api.nvim_set_keymap('t', '<M-j>', '<C-w>j', { noremap = true })
@@ -18,4 +19,7 @@ vim.api.nvim_set_keymap('n', '<M-h>', '<C-w>h', { noremap = true })
 vim.api.nvim_set_keymap('n', '<M-j>', '<C-w>j', { noremap = true })
 vim.api.nvim_set_keymap('n', '<M-k>', '<C-w>k', { noremap = true })
 vim.api.nvim_set_keymap('n', '<M-l>', '<C-w>l', { noremap = true })
-
+vim.api.nvim_set_keymap('n', '<M-H>', ':vertical resize -10 <CR>i', {noremap = true})
+vim.api.nvim_set_keymap('n', '<M-L>', ':vertical resize +10 <CR>i', {noremap = true})
+vim.api.nvim_set_keymap('n', '<M-K>', ':resize +10 <CR>i', {noremap = true})
+vim.api.nvim_set_keymap('n', '<M-J>', ':resize -10 <CR>i', {noremap = true})
