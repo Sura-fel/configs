b38bb0be78b712bebfab4c25adaf80cc95348e09
Surafel <fkresurafel@gmail.com>
2024-01-02 15:06:21 +0300

    removed ?

--- lua/configs/rust_tools_config.lua
+++ lua/configs/rust_tools_config.lua
@@ -10,15 +10,7 @@ rt.setup({
             },
         },
         on_attach = function(_, bufnr)
-            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr, desc = "Hover Actions (RT)" })
-            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr, desc = "Code [A]ction Group" })
             vim.keymap.set("n", "<leader>r", ":RustRun<CR>", { buffer = bufnr, desc = "[R]un Rust code" })
         end,
     },
-    tools = {
-        hover_actions = {
-            auto_focus = true
-        }
-    }
 })
-
