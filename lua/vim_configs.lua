7ee996db8330b9669068d9c2d7800528ee137687
Surafel <fkresurafel@gmail.com>
2024-01-02 14:46:29 +0300

    organized code

--- lua/vim_configs.lua
+++ lua/vim_configs.lua
@@ -8,18 +8,48 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.clipboard = 'unnamedplus'
-vim.api.nvim_set_keymap('n', '<Leader>t', ':botright split +<CR>:resize 10<CR>:terminal powershell -NoLogo<CR>i', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>T', ':botright vsplit +<CR>:terminal powershell -NoLogo<CR>i', {noremap = true})
-vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
+
+-- <Leader>t => powershell split to the bottom with length 10
+-- <Leader>T => powershell vsplit to right with half length
+vim.api.nvim_set_keymap(
+    'n', '<Leader>t',
+    ':botright split +<CR>:resize 10<CR>:terminal powershell -NoLogo<CR>i',
+    {
+        desc = "open [t]erminal in the bottom",
+        noremap = true
+    }
+)
+vim.api.nvim_set_keymap(
+    'n', 
+    '<Leader>T', 
+    ':botright vsplit +<CR>:terminal powershell -NoLogo<CR>i',
+    {
+        desc = "open [T]erminal in the right",
+        noremap = true
+    }
+)
+
+-- <esc> in terminal mode == <esc> in other mode
+vim.api.nvim_set_keymap(
+    't', 
+    '<Esc>', 
+    '<C-\\><C-n>', 
+    {noremap = true}
+)
+
+-- <M-h/j/k/l> for terminal and normal mode => move between splits
 vim.api.nvim_set_keymap('t', '<M-h>', '<C-w>h', { noremap = true })
 vim.api.nvim_set_keymap('t', '<M-j>', '<C-w>j', { noremap = true })
 vim.api.nvim_set_keymap('t', '<M-k>', '<C-w>k', { noremap = true })
 vim.api.nvim_set_keymap('t', '<M-l>', '<C-w>l', { noremap = true })
+
 vim.api.nvim_set_keymap('n', '<M-h>', '<C-w>h', { noremap = true })
 vim.api.nvim_set_keymap('n', '<M-j>', '<C-w>j', { noremap = true })
 vim.api.nvim_set_keymap('n', '<M-k>', '<C-w>k', { noremap = true })
 vim.api.nvim_set_keymap('n', '<M-l>', '<C-w>l', { noremap = true })
-vim.api.nvim_set_keymap('n', '<M-H>', ':vertical resize -10 <CR>i', {noremap = true})
-vim.api.nvim_set_keymap('n', '<M-L>', ':vertical resize +10 <CR>i', {noremap = true})
-vim.api.nvim_set_keymap('n', '<M-K>', ':resize +10 <CR>i', {noremap = true})
-vim.api.nvim_set_keymap('n', '<M-J>', ':resize -10 <CR>i', {noremap = true})
+
+-- <M-H/J/K/L> => resize the current split
+vim.api.nvim_set_keymap('n', '<M-H>', ':vertical resize -10 <CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<M-L>', ':vertical resize +10 <CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<M-J>', ':resize +10 <CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<M-K>', ':resize -10 <CR>', {noremap = true})
